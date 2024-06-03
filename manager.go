09eeda94c203f7d4e8f3c0ba3411fad37d4af173
remy-z <remyszach@gmail.com>
2024-06-03 15:11:25 -0700

    domain go boom

--- manager.go
+++ manager.go
@@ -262,6 +262,7 @@ func (m *Manager) removeFromCurrentRoom(client *Client) {
 
 // return true to allow connection, false to dismiss
 func checkOrigin(r *http.Request) bool {
+	return true
 	origin := r.Header.Get("Origin")
 	switch origin {
 	case "https://www.socketchat.app":
